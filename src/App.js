61cec54266c36f067831e53a9f85f95c077dea6c
cindykandie <cindyjk6@gmail.com>
2023-02-28 19:58:22 +0300

    Changes based on review

--- src/App.js
+++ src/App.js
@@ -1,66 +1,66 @@
-import React, { useState, useRef, useEffect } from 'react'
-import TodoList from './components/TodoList'
-import './App.css'
-
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
+/* eslint-disable */
+import React, { useState, useRef } from 'react';
+import TodoList from './components/TodoList';
+import NavBar from './components/NavBar';
+import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
 
-  useEffect(()=>{
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    setTodos(storedTodos)
-  },[])
-
-  function toggleTodo(id){
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.completed = !todo.completed
-
-    setTodos(newTodos)
-
-  }
-  function editLabel(newText){
-    console.log('edit', newText)
-  }
-  function deleteTodo(id){
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-
-    const novTodos = newTodos.filter(item => item !== todo)
-    
-    setTodos(novTodos)
-    
+    setTodos(newTodos);
   }
 
-  function handleAddTodo(e){
-    const name = todoNameRef.current.value
-    if(name === "") return 
-    setTodos(prevTodos =>{
-      return [...prevTodos, {id: name, name: name, completed: false}]
-    })
-    todoNameRef.current.value = null
+  function deleteTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+
+    const novTodos = newTodos.filter((item) => item !== todo);
+
+    setTodos(novTodos);
   }
-  function clearCompleted(){
-    const newTodos = todos.filter((todo => !todo.completed))
-    setTodos(newTodos)
+
+  function handleAddTodo() {
+    const name = todoNameRef.current.value;
+    if (name === '') return;
+    setTodos((prevTodos) => [...prevTodos, { id: name, name, completed: false }]);
+    todoNameRef.current.value = null;
+  }
+  function clearCompleted() {
+    const newTodos = todos.filter(((todo) => !todo.completed));
+    setTodos(newTodos);
   }
 
   return (
-    <div className='main-body'>
-    <h1>todos</h1>
-    <div className='user-inputfield'>
-      <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>+</button>
-    </div>
-    <div className='rem-todos'>{todos.filter(todo => !todo.completed).length} left  do</div>
-    <TodoList todos={todos} toggleTodo ={toggleTodo} deleteTodo = {deleteTodo} editLabel = {editLabel}/>
-    <button onClick={clearCompleted} className="clear-btn">Clear completed</button>
+    <div className="main-body">
+      <NavBar />
+      <h1>todos</h1>
+      <div className="user-inputfield">
+        <input ref={todoNameRef} type="text" />
+        <button type="submit" onClick={handleAddTodo}>+</button>
+      </div>
+      <div className="rem-todos">
+        {todos.filter((todo) => !todo.completed).length}
+        {' '}
+        left  do
+      </div>
+      <TodoList
+        todos={todos}
+        toggleTodo={toggleTodo}
+        deleteTodo={deleteTodo}
+      />
+      <button
+        type="submit"
+        onClick={clearCompleted}
+        className="clear-btn"
+      >
+        Clear completed
+      </button>
     </div>
 
   );
--- src/components/todo.js
+++ src/components/todo.js
@@ -1,30 +1,33 @@
-import React from 'react'
-import './Todo.css'
+/* eslint-disable */
+import React from 'react';
+import './Todo.css';
 
-export default function Todo({todo, toggleTodo, deleteTodo, editLabel}) {
-    function handleTodoClick(){
-        toggleTodo(todo.id)
-    }
-    function handleTodoDelete(){
-      deleteTodo(todo.id)
-    }
-    function handleTextChange(){
-      editLabel();
-    }
+export default function Todo({
+  todo, toggleTodo, deleteTodo, editLabel,
+}) {
+  function handleTodoClick() {
+    toggleTodo(todo.id);
+  }
+  function handleTodoDelete() {
+    deleteTodo(todo.id);
+  }
+  function handleTextChange() {
+    editLabel();
+  }
 
   return (
     <li>
       <div>
-      <input type= "checkbox" checked = {todo.completed} onChange = {handleTodoClick}/>
-        <label contentEditable onChange={handleTextChange} >
-        {todo.name}
+        <input type="checkbox" checked={todo.completed} onChange={handleTodoClick} />
+        <label contentEditable onChange={handleTextChange}>
+          {todo.name}
         </label>
       </div>
       <div>
-        <span title='edit'>📝</span>
-        <span title='delete' onClick = {handleTodoDelete}>⛔</span>
+        <span title="edit">📝</span>
+        <span title="delete" onClick={handleTodoDelete}>⛔</span>
       </div>
-        
+
     </li>
-  )
+  );
 }
